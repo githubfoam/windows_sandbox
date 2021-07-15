4768aec26654fc572525a503616ac32ce86d2606
Your Name <you@example.com>
2021-07-15 12:17:19 +0300

    init

---
+++ scripts/Install_FireFox.ps1
@@ -0,0 +1,15 @@
+# Silent Install Firefox 
+## Download URL: https://www.mozilla.org/en-US/firefox/all/
+### When a new version of FireFox comes out, just change the firefox version or url to the new value
+
+#--------------------------------------------------------------------------------------------------------
+
+# Path for the Workdir
+$workdir = "C:\tmp\"
+
+# Check if work directory exists if not create it
+
+If (Test-Path -Path $workdir -PathType Container)
+{ Write-Host "$workdir already exists" -ForegroundColor Red}
+ELSE
+{ New-Item -Path $workdir  -ItemType directory }
