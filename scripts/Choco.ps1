ff6dc70ae23d929fb0e2e3f26d1fa0d66dd49fcb
Your Name <you@example.com>
2021-07-15 20:52:29 +0300

    condensons

---
+++ scripts/Choco.ps1
@@ -0,0 +1,14 @@
+# Silent Install Firefox 
+## Download URL: https://www.mozilla.org/en-US/firefox/all/
+### When a new version of FireFox comes out, just change the firefox version or url to the new value
+
+#--------------------------------------------------------------------------------------------------------
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/set-psdebug?view=powershell-7.1
+Set-PSDebug -Trace 2 #turns script debugging features on and off, sets the trace level
+
+$VerbosePreference = "continue"
+Write-Output $VerbosePreference
+#--------------------------------------------------------------------------------------------------------
+
+# https://docs.chocolatey.org/en-us/choco/commands/install
+choco install --yes --no-progress --virus-check sysinternals googlechrome 7zip ChocolateyGUI
